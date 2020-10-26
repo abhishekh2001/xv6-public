78097061d0a0317361394424327877d1243f2bcb
abhishekh <abhishekh2001@gmail.com>
2020-10-26 13:24:09 +0530

    minor

--- zombietest.c
+++ zombietest.c
@@ -8,11 +8,13 @@ int main(int argc, char** argv)
   int ch = fork();
 
   if (ch > 0){  /* parent */
-      exit();
+    wait();
+    printf(1, "Done parent\n");
+    exit();
   } else if(ch == 0){  /* child */
-    sleep(30);
+    sleep(300);
   } else {
-      printf(2, "fork failed\n");
+    printf(2, "fork failed\n");
   }
   exit();
 }
